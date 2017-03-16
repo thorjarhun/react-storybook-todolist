7a1f7f2438f512ed573200642ba6a406033a02f6
John Arthur <thorjarhun@gmail.com>
2017-03-15 23:29:03 -0500

    improve?? Body code. I'm not sure whether I like the Footer or Body subcomponent style better. Probably need to drop jsx to make it elegant.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Item from './Item';
 
+const ItemList = ({items, itemActions}) =>
+	<ul className="todo-list">
+		{
+			items.map(item =>
+				<Item key={item.id}
+				      item={item}
+					{...itemActions(item.id)}/>
+			)
+		}
+	</ul>;
+
 const Body = ({items, toggleAll, itemActions}) =>
 	<section className="main">
 		<input className="toggle-all"
 		       type="checkbox"
 		       checked={!!items.length && items.every(({completed}) => completed)}
 		       onChange={toggleAll} />
-		<ul className="todo-list">
-			{
-				items.map(item =>
-					<Item key={item.id}
-					      item={item}
-						{...itemActions(item.id)}/>
-				)
-			}
-		</ul>
+		<ItemList {...{items}} {...{itemActions}} />
 	</section>;
 
 export default Body;
